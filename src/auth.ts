2be50144aba3b4b07ead077bb23b6a3c8df7cb38
ghedjati oussama <ouss.ghedje@gmail.com>
2026-05-15 01:45:27 +0200

    Explicitly set AUTH_SECRET in config

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth-config";
 
-export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  ...authConfig,
+  secret: process.env.AUTH_SECRET,
+});
